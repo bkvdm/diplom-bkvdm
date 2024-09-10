d785ba13d51a03c87cc0bcf8e4ea3e97cabf87e4
bkvdm <vadim@991199.ru>
2024-09-10 22:52:04 +0300

    Дополнены класс UserDto.

--- src/main/java/ru/skypro/homework/dto/UserDto.java
+++ src/main/java/ru/skypro/homework/dto/UserDto.java
@@ -1,4 +1,21 @@
 package ru.skypro.homework.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.skypro.homework.model.ImageUser;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
+
+    private int id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private Role role;
+    private String username;
+    private ImageUser image;
 }
